1f4811a5e5f301b1bcc8cbfd970fcf0e66fd634d
ksimback <kevin.simback@gmail.com>
2016-06-02 19:39:43 -0700

    Update script.js

--- script.js
+++ script.js
@@ -29,8 +29,8 @@ Is that OK? %[Yes](postback:yes) %[No](postback:no)`))
     finish: {
         receive: (bot, message) => {
             return bot.getProp('name')
-                .then((name) => bot.say(`Sorry ${name}, my creator didn't ` +
-                        'teach me how to do anything else!'))
+                .then((name) => bot.say(`Sorry ${name}, wine didn't ` +
+                        'teach me how to do this!'))
                 .then(() => 'finish');
         }
     }
