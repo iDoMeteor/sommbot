065a3fadcaec6eb1ecd3c157e2ce7e76d5824d28
ksimback <ksimback@users.noreply.github.com>
2016-06-04 23:24:27 -0400

    Update script.js

--- script.js
+++ script.js
@@ -23,8 +23,8 @@ module.exports = new Script({
         receive: (bot, message) => {
             const name = message.text;
             return bot.setProp('name', name)
-                .then(() => bot.say(`Great! I'll call you ${name}
-Is that OK? %[Yes](postback:yes) %[No](postback:no)`))
+                .then(() => bot.say(`Great! Thanks ${name},
+Do you like wine? %[Yes](postback:yes) %[No](postback:no)`))
                 .then(() => 'finish');
         }
     },
