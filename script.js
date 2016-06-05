b0aeaf59ff81361adb97cff31f3e7a9472437443
Kevin Simback <kevin.simback@gmail.com>
2016-06-05 13:59:09 -0400

    Added speak function

--- script.js
+++ script.js
@@ -13,10 +13,60 @@ module.exports = new Script({
 
     start: {
         receive: (bot) => {
-            return bot.say('Hi! I\'m SommBot, your personal digital sommelier!')
-                .then(() => 'askName');
+            return bot.say('Hi! I\'m SommBot, your personal digital sommelier! Say HELLO to get started.')
+                .then(() => 'speak');
         }
     },
+
+    speak: {
+        receive: (bot, message) => {
+
+            let upperText = message.text.trim().toUpperCase();
+
+            function updateSilent() {
+                switch (upperText) {
+                    case "CONNECT ME":
+                        return bot.setProp("silent", true);
+                    case "DISCONNECT":
+                        return bot.setProp("silent", false);
+                    default:
+                        return Promise.resolve();
+                }
+            }
+
+            function getSilent() {
+                return bot.getProp("silent");
+            }
+
+            function processMessage(isSilent) {
+                if (isSilent) {
+                    return Promise.resolve("speak");
+                }
+
+                if (!_.has(scriptRules, upperText)) {
+                    return bot.say(`I didn't understand that.`).then(() => 'speak');
+                }
+
+                var response = scriptRules[upperText];
+                var lines = response.split('\n');
+
+                var p = Promise.resolve();
+                _.each(lines, function(line) {
+                    line = line.trim();
+                    p = p.then(function() {
+                        console.log(line);
+                        return bot.say(line);
+                    });
+                })
+
+                return p.then(() => 'speak');
+            }
+
+            return updateSilent()
+                .then(getSilent)
+                .then(processMessage);
+        }
+    }
 
     askName: {
         prompt: (bot) => bot.say('What\'s your name?'),
