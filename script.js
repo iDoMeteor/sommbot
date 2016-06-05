d4a771ad6615c452c74995708fa64d95f2a367c3
Kevin Simback <kevin.simback@gmail.com>
2016-06-05 16:46:57 -0400

    lodash update

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 'use strict';
 
-//const _ = require('lodash');
+const _ = require('lodash');
 const Script = require('smooch-bot').Script;
 
 const scriptRules = require('./script.json');
@@ -18,55 +18,55 @@ module.exports = new Script({
         }
     },
 
-    //speak: {
-    //    receive: (bot, message) => {
+    speak: {
+        receive: (bot, message) => {
 
-      //      let upperText = message.text.trim().toUpperCase();
+            let upperText = message.text.trim().toUpperCase();
 
-        //    function updateSilent() {
-          //      switch (upperText) {
-            //        case "CONNECT ME":
-              //          return bot.setProp("silent", true);
-                //    case "DISCONNECT":
-                  //      return bot.setProp("silent", false);
-                    //default:
-                      //  return Promise.resolve();
-                //}
-            //}
+            function updateSilent() {
+                switch (upperText) {
+                    case "CONNECT ME":
+                        return bot.setProp("silent", true);
+                    case "DISCONNECT":
+                        return bot.setProp("silent", false);
+                    default:
+                        return Promise.resolve();
+                }
+            }
 
-            //function getSilent() {
-              //  return bot.getProp("silent");
-            //}
+            function getSilent() {
+                return bot.getProp("silent");
+            }
 
-            //function processMessage(isSilent) {
-              //  if (isSilent) {
-                //    return Promise.resolve("speak");
-                //}
+            function processMessage(isSilent) {
+                if (isSilent) {
+                    return Promise.resolve("speak");
+                }
 
-                //if (!_.has(scriptRules, upperText)) {
-                  //  return bot.say(`I didn't understand that.`).then(() => 'speak');
-                //}
+                if (!_.has(scriptRules, upperText)) {
+                    return bot.say(`I didn't understand that.`).then(() => 'speak');
+                }
 
-                //var response = scriptRules[upperText];
-                //var lines = response.split('\n');
+                var response = scriptRules[upperText];
+                var lines = response.split('\n');
 
-                //var p = Promise.resolve();
-                //_.each(lines, function(line) {
-                  //  line = line.trim();
-                    //p = p.then(function() {
-                      //  console.log(line);
-                        //return bot.say(line);
-                    //});
-                //})
+                var p = Promise.resolve();
+                _.each(lines, function(line) {
+                    line = line.trim();
+                    p = p.then(function() {
+                        console.log(line);
+                        return bot.say(line);
+                    });
+                })
 
-                //return p.then(() => 'speak');
-            //}
+                return p.then(() => 'speak');
+            }
 
-            //return updateSilent()
-              //  .then(getSilent)
-                //.then(processMessage);
-        //}
-    //}
+            return updateSilent()
+                .then(getSilent)
+                .then(processMessage);
+        }
+    }
 
     askName: {
         prompt: (bot) => bot.say('What\'s your name?'),
