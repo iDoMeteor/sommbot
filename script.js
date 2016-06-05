8a6494b0e70bb080e324fd10ae616952fda684dc
Kevin Simback <kevin.simback@gmail.com>
2016-06-05 14:04:28 -0400

    Comment out speak function

--- script.js
+++ script.js
@@ -14,59 +14,59 @@ module.exports = new Script({
     start: {
         receive: (bot) => {
             return bot.say('Hi! I\'m SommBot, your personal digital sommelier! Say HELLO to get started.')
-                .then(() => 'speak');
+                .then(() => 'askName');
         }
     },
 
-    speak: {
-        receive: (bot, message) => {
+    //speak: {
+    //    receive: (bot, message) => {
 
-            let upperText = message.text.trim().toUpperCase();
+      //      let upperText = message.text.trim().toUpperCase();
 
-            function updateSilent() {
-                switch (upperText) {
-                    case "CONNECT ME":
-                        return bot.setProp("silent", true);
-                    case "DISCONNECT":
-                        return bot.setProp("silent", false);
-                    default:
-                        return Promise.resolve();
-                }
-            }
+        //    function updateSilent() {
+          //      switch (upperText) {
+            //        case "CONNECT ME":
+              //          return bot.setProp("silent", true);
+                //    case "DISCONNECT":
+                  //      return bot.setProp("silent", false);
+                    //default:
+                      //  return Promise.resolve();
+                //}
+            //}
 
-            function getSilent() {
-                return bot.getProp("silent");
-            }
+            //function getSilent() {
+              //  return bot.getProp("silent");
+            //}
 
-            function processMessage(isSilent) {
-                if (isSilent) {
-                    return Promise.resolve("speak");
-                }
+            //function processMessage(isSilent) {
+              //  if (isSilent) {
+                //    return Promise.resolve("speak");
+                //}
 
-                if (!_.has(scriptRules, upperText)) {
-                    return bot.say(`I didn't understand that.`).then(() => 'speak');
-                }
+                //if (!_.has(scriptRules, upperText)) {
+                  //  return bot.say(`I didn't understand that.`).then(() => 'speak');
+                //}
 
-                var response = scriptRules[upperText];
-                var lines = response.split('\n');
+                //var response = scriptRules[upperText];
+                //var lines = response.split('\n');
 
-                var p = Promise.resolve();
-                _.each(lines, function(line) {
-                    line = line.trim();
-                    p = p.then(function() {
-                        console.log(line);
-                        return bot.say(line);
-                    });
-                })
+                //var p = Promise.resolve();
+                //_.each(lines, function(line) {
+                  //  line = line.trim();
+                    //p = p.then(function() {
+                      //  console.log(line);
+                        //return bot.say(line);
+                    //});
+                //})
 
-                return p.then(() => 'speak');
-            }
+                //return p.then(() => 'speak');
+            //}
 
-            return updateSilent()
-                .then(getSilent)
-                .then(processMessage);
-        }
-    }
+            //return updateSilent()
+              //  .then(getSilent)
+                //.then(processMessage);
+        //}
+    //}
 
     askName: {
         prompt: (bot) => bot.say('What\'s your name?'),
